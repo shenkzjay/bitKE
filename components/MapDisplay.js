eb4aa6b8e363ea9eae6db71214a71b1f2cae6f4e
Chukwuemeka Wisdom <guruwiz2@gmail.com>
2021-11-25 12:25:10 +0100

    Fix: onMouseLeave popup close

--- components/MapDisplay.js
+++ components/MapDisplay.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react"
+import { React, useEffect, useState, useRef } from "react"
 import axios from "axios"
 import Style from "../styles/Markerstyle.module.css"
 import { MapContainer, TileLayer, Marker, Tooltip, Popup } from "react-leaflet"
@@ -10,40 +10,13 @@ import Head from "next/head"
 import commaNumber from 'comma-number'
 
 // div icon 
-import L from 'leaflet'
+import L, { marker } from 'leaflet'
 
 
 
 
 // init MapDisplay
 function MapDisplay() {
-  const [viewport, setViewport] = useState({
-    latitude: 7.1881,
-    longitude: 21.0936,
-    zoom: 2.6,
-    width: '100vw',
-    height: '100vh',
-    scrollWheelZoom: 'false'
-  })
-
-
-  // init showToolTip state
-  const [showToolTip, setShowToolTip] = useState(false)
-
-  useEffect(() => {
-    (async function () {
-      const response = await axios.get(`${process.env.ROOT_API_BASE_URL}/fetch/stats`)
-
-      if (!response.data.success) {
-        return console.log("error occured")
-
-      }
-      console.log(response.data.data)
-      setStats(response.data.data)
-    })()
-  }, [])
-
-
 
   //init markerIcon
   const markerIcon = new L.divIcon({
@@ -56,12 +29,63 @@ function MapDisplay() {
   });
 
 
+
+  const [viewport, setViewport] = useState({
+    latitude: 7.1881,
+    longitude: 21.0936,
+    zoom: 2.6,
+    width: '100vw',
+    height: '100vh',
+    scrollWheelZoom: 'false'
+  })
+
+  // init allStats state
   const [allStats, setStats] = useState([])
+
+
+  // init useEffect
+  useEffect(() => {
+    (async function () {
+      const response = await axios.get(`${process.env.ROOT_API_BASE_URL}/fetch/stats`)
+
+      if (!response.data.success) {
+        return console.log("error occured")
+
+      }
+      console.log(response.data.data)
+      setStats(response.data.data)
+    })()
+  }, [setStats])
+
+
+
+  // init displayPopup state
+  const [displayPopup, setDisplayPopup] = useState(false)
+
+  // init markerRef 
+  const markerRef = useRef(null)
+
+
+  // init openPopup func
+  const handleOpenPopup = (e) => {
+       e.target.openPopup()
+
+       markerRef.current = e
+  }
+
+
+  const handleClosePopup = () => {
+    markerRef.current.target.closePopup()
+  }
+
+
+
+
   return (
     <div className={Style.MapBody}>
       <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-        </Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+      </Head>
       <MapContainer
         center={[viewport.latitude, viewport.longitude]}
         zoom={viewport.zoom}
@@ -77,15 +101,18 @@ function MapDisplay() {
 
 
         {allStats.length > 0 && allStats.map((stat, index) => {
-        
-      return <Marker eventHandlers={{ mouseover: (e) =>  e.target.openPopup()}}   onClick={() => console.log("Hello")} key={index} position={[stat.latitude, stat.longitude]} icon={markerIcon}>
-      {/* // return <Marker onMouseOver={e => {e.target.openPopup()}}  onMouseOut={e => {e.target.closePopup()}}  key={index} position={[stat.latitude, stat.longitude]} icon={markerIcon}> */}
+
+          return <Marker eventHandlers={{ mouseover: (e) => handleOpenPopup(e) }} key={index} position={[stat.latitude, stat.longitude]} icon={markerIcon}>
+            {/* // return <Marker onMouseOver={e => {e.target.openPopup()}}  onMouseOut={e => {e.target.closePopup()}}  key={index} position={[stat.latitude, stat.longitude]} icon={markerIcon}> */}
             <Popup className={Style.popup_data}>
-                <h1>{stat.country}</h1>
-                <p>Population: {commaNumber(stat.population)}</p>
-                <p>Crypto Users: {commaNumber(stat.numberOfCryptoOwners)}</p>
-                <p>People with internet Connection: {commaNumber(JSON.parse(stat.peopleWithInternetConnection).peopleWithInternet)}</p>
+            <div onMouseLeave={() => handleClosePopup()}>
+              <h1>{stat.country}</h1>
+              <p>Population: {commaNumber(stat.population)}</p>
+              <p>Crypto Users: {commaNumber(stat.numberOfCryptoOwners)}</p>
+              <p>People with internet Connection: {commaNumber(JSON.parse(stat.peopleWithInternetConnection).peopleWithInternet)}</p>
+              </div>
             </Popup>
+            
           </Marker>
         })}
 
